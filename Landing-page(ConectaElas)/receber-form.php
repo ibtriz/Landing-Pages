d82dc3d6c256366a3f52df061b48fd8cc4fb7803
Beatriz Francelino <beatriz.fbcarneiro@gmail.com>
2022-04-07 11:44:48 -0300

    form.php

--- Landing-page(ConectaElas)/receber-form.php
+++ Landing-page(ConectaElas)/receber-form.php
@@ -1,5 +1,11 @@
 <?php
-$nome = $_POST['nome'];
-echo "Olá " . $nome;
-$nome = $_REQUEST['nome'];
+$nome= $_POST['fname'];
+$email = $_POST['femail'];
+$mensagem= $_POST['message'];
+$formcontent="Mensagem enviada por: $name n Texto: $message";
+$recipient = "seu-email-aqui@gmail.com";
+$subject = "Formulário de contato";
+$mailheader = "De: $email rn";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Erro!");
+echo "Obrigado por entrar em contato!";
 ?>
